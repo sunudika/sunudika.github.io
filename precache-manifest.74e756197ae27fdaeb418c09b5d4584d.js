96332d32985272ade3ae4d1f58a30978bf48ab1b
Sunu Ilham Pradika <sunudika@gmail.com>
2020-06-20 15:33:45 +0700

    Copy build folder

---
+++ precache-manifest.74e756197ae27fdaeb418c09b5d4584d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a6c7e3f43dcf12d975a",
+    "url": "/static/css/main.2d12f9dd.chunk.css"
+  },
+  {
+    "revision": "4a6c7e3f43dcf12d975a",
+    "url": "/static/js/main.4a6c7e3f.chunk.js"
+  },
+  {
+    "revision": "fe125aa42a0e0c7c4d34",
+    "url": "/static/js/1.fe125aa4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cfc8623431f264fad4f136248a4c0aa9",
+    "url": "/index.html"
+  }
+];
